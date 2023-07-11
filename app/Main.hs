ea86ef787e13bfc2a2806aa73620aae45eb39551
ArtMGreen <artyom5145@gmail.com>
2023-07-11 20:13:15 +0300

    test commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,18 @@
 module Main where
 
-a :: Int
-a = 5
+import Codec.Picture
+
+
+picturePath :: FilePath 
+picturePath = "/home/artmgreen/Pictures/poolrooms_bg.jpg"
+
+tackleImage :: (Pixel a) => Image a -> a
+tackleImage img = pixelAt img 5 5
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    esdi <- readImage picturePath
+    case esdi of
+        Left error -> putStrLn error
+        Right dynImg -> print $ tackleImage (convertRGB8 dynImg)
+        
